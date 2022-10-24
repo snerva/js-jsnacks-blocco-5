c3b17deb94e8ebcb7c665954b8dd13513befb7e0
snerva <selenenerva@live.it>
2022-10-24 19:33:03 +0200

    snack 5

---
+++ snack_5/main.js
@@ -0,0 +1,18 @@
+//scrivi una function che accetti tre arguments: un array e due numbers (a < b e b >= array.lenght)
+//function return newArray con i valori che hanno posizione compresa tra a e b
+const list = ['qui', 'quo', 'qua', 'zio paperino', 'zio paperone'];
+const a = 2;
+const b = 4;
+
+const newList = mix()
+
+function mix(){
+    let newList = [];
+    list.forEach(i => {
+        console.log(i);
+        if(i > a && i < b){
+            
+          
+        }
+    });
+}
